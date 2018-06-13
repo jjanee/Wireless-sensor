d83e0fefd8f9de48ec30f72be920a80b42cb68d5
jjanee <58070019@kmitl.ac.th>
2018-06-13 21:07:30 +0700

    had line
    มีเส้นแล้ว แต่clusterตัวแรกต่อกับทุกnode

--- create_point.py
+++ create_point.py
@@ -4,20 +4,21 @@ from math import sqrt
 
 def create_point():
     "insert area and population of node and point of base station"
-    area_x = int(5) #meter
-    area_y = int(5) #meter
-    num_base = int(3)
+    area_x = int(100) #meter
+    area_y = int(100) #meter
+    num_base = int(1)
     base_x, base_y, station = [], [], []
     ran_nodeX, ran_nodeY = [],[]
     node_member, cluster_member = [],[]
     list_distance = []
-    density = float(0.5) #nodes/meter^2
+    density = float(0.025) #nodes/meter^2
     len_nodes = int(density*(area_x*area_y))
 
-    for item in range (0, num_base):
-        base_x.append(int(input("Enter point x of base station "+str(item+1)+"= ")))
-        base_y.append(int(input("Enter point y of base station "+str(item+1)+"= ")))
+    for item in range (0, num_base): #input base station point
+        base_x.append(int(input("Enter point x of base station "+str(item+1)+" = ")))
+        base_y.append(int(input("Enter point y of base station "+str(item+1)+" = ")))
         station.append([base_x[item], base_y[item]])
+
     count = 0
     while(len(node_member) != len_nodes): #random Node
         ran_nodeX.append(rd.randint(0,area_x))
@@ -27,30 +28,38 @@ def create_point():
             node_member.append([ran_nodeX[count], ran_nodeY[count]])
         count += 1
 
-    while(len(cluster_member) != 3): #random Cluster from amount Node
-        temp1e = node_member[rd.randint(0,len(node_member)-1)]
-        if temp1e not in cluster_member:
-            cluster_member.append(temp1e)   
+    while(len(cluster_member) != 10): #random Cluster from amount Node
+        cluster = node_member[rd.randint(0,len(node_member)-1)]
+        if cluster not in cluster_member:
+            cluster_member.append(cluster)
+            node_member.remove(cluster)
 
-    for x in range(len(node_member)):
+    for node in range(len(node_member)):
         near_cluster = "none"
-        for x2 in range(len(cluster_member)):
-            cal_distance = sqrt((node_member[x][0] - cluster_member[x2][0])**2\
-                                + (node_member[x][1] - cluster_member[x2][1])**2)
+        for cluster in range(len(cluster_member)):
+            cal_distance = sqrt((node_member[node][0] - cluster_member[cluster][0])**2+\
+                                (node_member[node][1] - cluster_member[cluster][1])**2)
             
             #find shortest cluster **if cal_distance = 0 that's a Cluster
-            if near_cluster == "none":
+            if near_cluster == "none" :
                 near_cluster = cal_distance
-                clus_num = x2
-            elif cal_distance < near_cluster:
+                clus_num = cluster
+                plt.plot([node_member[node][0], cluster_member[cluster][0]],\
+                         [node_member[node][1], cluster_member[cluster][1]],\
+                         linestyle='-', linewidth=0.1)#ไปทุกโหนด
+            if cal_distance < near_cluster:
                 near_cluster = cal_distance
-                clus_num = x2
+                clus_num = cluster
+                plt.plot([node_member[node][0], cluster_member[cluster][0]],\
+                         [node_member[node][1], cluster_member[cluster][1]],\
+                         color='k', linestyle='-', linewidth=0.1)
 
-    clu_x, clu_y = zip(*cluster_member)
-    nod_x, nod_y = zip(*node_member)
+    clus_x, clus_y = zip(*cluster_member)
+    node_x, node_y = zip(*node_member)
+    base_x, base_y = zip(*station)
     plt.plot(base_x[0:], base_y[0:],'ro')#nodes
-    plt.plot(nod_x[0:], nod_y[0:],'bo')#nodes
-    plt.plot(clu_x[0:] , clu_y[0:], 'go')#cluster
+    plt.plot(node_x[0:], node_y[0:],'bo')#nodes
+    plt.plot(clus_x[0:], clus_y[0:], 'go')#cluster head
     plt.show()
     
 create_point()
