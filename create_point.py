058447cad08ff7cff087680e5134f35401f8d177
jjanee <58070019@kmitl.ac.th>
2018-06-22 01:10:26 +0700

    u

--- create_point.py
+++ create_point.py
@@ -24,28 +24,35 @@ def create_point():
     while(len(node_member) != len_nodes): #random Node
         ran_nodeX.append(rd.randint(0,area_x))
         ran_nodeY.append(rd.randint(0,area_y))
-        node_member.append([ran_nodeX[count], ran_nodeY[count]])
         if [ran_nodeX[count], ran_nodeY[count]] not in node_member and \
-           [ran_nodeX[count], ran_nodeY[count]] not in station and\
-           count == 2:
+          [ran_nodeX[count], ran_nodeY[count]] not in station :
             node_member.append([ran_nodeX[count], ran_nodeY[count]])
-            cal_distance = math.sqrt((ran_nodeX[count] - ran_nodeX[count-1])**2+\
-                                (ran_nodeY[count] - ran_nodeY[count-1])**2)
-            print("cal = "+str(cal_distance))
         count += 1
         
-        
+    count = 0    
     while(len(cluster_member) != population): #random Cluster from amount Node
         cluster = node_member[rd.randint(0,len(node_member)-1)]
-        if cluster not in cluster_member :
+        
+        if count < 2 and cluster not in cluster_member:
             cluster_member.append(cluster)
             node_member.remove(cluster)
-    
-    for node in range(len(node_member)):
+        elif cluster not in cluster_member :
+            cluster_member.append(cluster)
+            node_member.remove(cluster)
+        cal_distance = math.sqrt((cluster_member[count:][0] - cluster_member[count-1][0])**2+\
+                                (cluster_member[count:][1] - cluster_member[count-1][1])**2)
+        print(str(count)+" cal "+str(cluster_member[count])+\
+                      str(cluster_member[count-1])+"= "+str(cal_distance))
+                
+        
+        count += 1
+        
+    print(cluster_member)
+   # for node in range(len(node_member)):
 ##        near_cluster = "none"
-        for cluster in range(len(cluster_member)):
-            cal_distance = math.sqrt((node_member[node][0] - cluster_member[cluster][0])**2+\
-                                     (node_member[node][1] - cluster_member[cluster][1])**2)
+        #for cluster in range(len(cluster_member)):
+            #cal_distance = math.sqrt((node_member[node][0] - cluster_member[cluster][0])**2+\
+            #                         (node_member[node][1] - cluster_member[cluster][1])**2)
 
         
 ##       plt.plot([node_member[node][0], cluster_member[cluster][0]],\
@@ -68,8 +75,6 @@ def create_point():
     clus_x, clus_y = zip(*cluster_member)
     node_x, node_y = zip(*node_member)
     base_x, base_y = zip(*station)
-    print(base_x)
-    print(base_y)
     plt.plot(base_x[0:], base_y[0:],'ro')#nodes
     plt.plot(node_x[0:], node_y[0:],'bo')#nodes
     plt.plot(clus_x[0:], clus_y[0:], 'go')#cluster head
