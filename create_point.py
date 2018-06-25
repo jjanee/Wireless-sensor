db4b02e91568f3a2995e4ecbf8018f1173ef68cf
jjanee <58070019@kmitl.ac.th>
2018-06-26 00:30:41 +0700

    sum

--- create_point.py
+++ create_point.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import random as rd
-import numpy 
 import math 
 
 def create_point():
@@ -8,7 +7,7 @@ def create_point():
 ##    area_x = int(input("Enter width area = ")) #meter
 ##    area_y = int(input("Enter height area = ")) #meter
 ##    num_base = int(input(" How many base sation in area = "))
-    area_x, area_y = 100, 100
+    area_x, area_y = 40, 40
     station, ran_nodeX, ran_nodeY  = [[1,1]],[],[]
     node_member, cluster_member = [],[]
     distance, node_distance = [], []
@@ -26,34 +25,41 @@ def create_point():
         if node not in node_member and node not in station :
             node_member.append(node)
    
-    
     while(len(cluster_member) != population): #random Cluster from amount Node
         cluster = node_member[rd.randint(0,len(node_member)-1)]
         cluster_member.append(cluster)
         node_member.remove(cluster)
-    
+
     for node in range(len(node_member)):
         near_cluster = "none"
+        max_member = 0
         for cluster in range(len(cluster_member)):
             cal_distance = math.sqrt((node_member[node][0] - cluster_member[cluster][0])**2+\
                                      (node_member[node][1] - cluster_member[cluster][1])**2)
             if near_cluster == "none"  :#ไปทุกโหนด
                 near_cluster = cal_distance
                 clus_num = cluster
-            elif cal_distance < near_cluster and node_member[node] not in distance : 
+            elif cal_distance < near_cluster and \
+                 node_member[node] not in distance and \
+                 max_member == 0: 
                 near_cluster = cal_distance
                 clus_num = cluster
-                distance.append([node_member[node], cluster_member[clus_num]])
-                
-        plt.plot([node_member[node][0], cluster_member[clus_num][0]],\
-                [node_member[node][1], cluster_member[clus_num][1]],\
-                color='k', linestyle='-', linewidth=0.1)#สีดำ
-    print(distance ,"/n")
+                distance.append([cluster_member[clus_num],node_member[node]])
+        plt.plot([distance[node][0][0] ,distance[clus_num][1][0]],\
+                 [distance[node][0][1] ,distance[clus_num][1][1]],\
+                 color='k', linestyle='-', linewidth=0.1)
+##        plt.plot([node_member[node][0], cluster_member[clus_num][0]],\
+##                [node_member[node][1], cluster_member[clus_num][1]],\
+##                color='k', linestyle='-', linewidth=0.1)#สีดำ 
+
     clus_x, clus_y = zip(*cluster_member)
     node_x, node_y = zip(*node_member)
     base_x, base_y = zip(*station)
+    plt.title('Node')
+    plt.xlabel('Widt')
+    plt.ylabel('Height')
     plt.plot(base_x[0:], base_y[0:],'ro')#nodes
-    plt.plot(node_x[0:], node_y[0:],'bo')#nodes
+    plt.plot(node_x[0:], node_y[0:],'.')#nodes
     plt.plot(clus_x[0:], clus_y[0:], 'go')#cluster head
     plt.show()
     
