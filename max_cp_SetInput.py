63297340282b623ea84968fbaeb5e35d978330c1
max11357 <max7456@gmail.com>
2018-06-28 16:09:18 +0700

    Move plot line out of cal_dis loop
    1.make it easy to move to new function
    2.new variable -> shot_dis_data
       - save shot distance and what Cluster
          do Nodes select

--- max_cp_SetInput.py
+++ max_cp_SetInput.py
@@ -5,24 +5,28 @@ import math
 def whole_system(width, height, density, clus_density, num_base):
     "insert area and population of node and point of base station"
     
-    node_member, cluster_member, station = [], [], [] 
+    #variables
+    node_member, cluster_member, station, shot_dis_data = [], [], [], []
     len_nodes = math.ceil(density*(width*height))
     len_cluster = math.ceil(clus_density*len_nodes)
-    for item in range (0, num_base): #input base station point
-        station.append(map(int,"12,3".split(',')))
+    #input base station point
+    for item in range(num_base): 
+        station.append(map(int,"51,-1".split(',')))
 
-    
+
+    #random Node
     count = 0
-    while(len(node_member) != len_nodes): #random Node
+    while(len(node_member) != len_nodes): 
         ran_nodex, ran_nodey = rd.randint(0,width), rd.randint(0,height)
-        if [ran_nodex, ran_nodey] not in node_member and \
-        [ran_nodex, ran_nodey] not in station :
+        if [ran_nodex, ran_nodey] not in node_member and\
+           [ran_nodex, ran_nodey] not in station :
             node_member.append([ran_nodex, ran_nodey])
         count += 1
 
-        
+
+    #random Cluster from amount Node  
     count = 0    
-    while(len(cluster_member) != len_cluster): #random Cluster from amount Node
+    while(len(cluster_member) != len_cluster): 
         cluster = node_member[rd.randint(0,len(node_member)-1)]
         
         if count < 2 and cluster not in cluster_member:
@@ -32,7 +36,13 @@ def whole_system(width, height, density, clus_density, num_base):
             cluster_member.append(cluster)
             node_member.remove(cluster)
         count += 1
-        
+    
+    
+    #split 2-metric list to 1-metric list
+    clus_x, clus_y = zip(*cluster_member)
+    node_x, node_y = zip(*node_member)
+    base_x, base_y = zip(*station)
+    
         
     #find distance between node and cluster
     for node in range(len(node_member)):
@@ -48,15 +58,15 @@ def whole_system(width, height, density, clus_density, num_base):
             elif cal_distance < shot_dis:
                 shot_dis = cal_distance
                 what_clus = cluster
-        #create line between node and cluster
-        plt.plot([node_member[node][0], cluster_member[what_clus][0]],\
-                 [node_member[node][1], cluster_member[what_clus][1]],\
-                 color='k', linestyle='-', linewidth=0.1)#สีดำ
-
+        shot_dis_data.append([node, what_clus, shot_dis])
     
-    clus_x, clus_y = zip(*cluster_member)
-    node_x, node_y = zip(*node_member)
-    base_x, base_y = zip(*station)
+    
+    #plot line between node and cluster
+    for z in range(len(shot_dis_data)):
+        plt.plot([node_member[shot_dis_data[z][0]][0], cluster_member[shot_dis_data[z][1]][0]],\
+                 [node_member[shot_dis_data[z][0]][1], cluster_member[shot_dis_data[z][1]][1]],\
+                 color='k', linestyle='-', linewidth=0.1)#สีดำ
+    #plot node, base, cluster
     plt.plot(base_x[0:], base_y[0:],'ro')#base station
     plt.plot(node_x[0:], node_y[0:],'bo')#nodes
     plt.plot(clus_x[0:], clus_y[0:],'go')#cluster head
