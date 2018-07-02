4f4e58f86135203987f80627688633f8d1427afb
max11357 <max7456@gmail.com>
2018-07-02 16:59:07 +0700

    do csv (not done)

--- max_cp_SetInput.py
+++ max_cp_SetInput.py
@@ -1,3 +1,4 @@
+import csv
 import matplotlib.pyplot as plt
 import random as rd
 import math
@@ -5,52 +6,54 @@ import math
 
 def variable(width, height, density, cluster_density):
     """variables"""
-    node_member, cluster_member, station, shot_dis_data = [], [], [], []
+    node_member, cluster_member, station_member, shot_dis_data = [], [], [], []
     len_nodes = math.ceil(density * (width * height))
     len_cluster = math.ceil(cluster_density * len_nodes)
+    return node_member, cluster_member, station_member, shot_dis_data, len_nodes, len_cluster
 
-    return node_member, cluster_member, station, shot_dis_data, len_nodes, len_cluster
 
-
-def base_station(num_base, station):
+def base_station(num_base, station_member):
     """input base station point"""
     for item in range(num_base):
-        station.append(map(int, "51,-1".split(',')))
-    # split 2d list to 1d list
-    base_x, base_y = zip(*station)
-    return station, base_x, base_y
+        station_member.append(list(map(int, "51,-1".split(','))))
+    # append data to csv. file
+    with open('station_member.csv', 'w', newline='') as csvnew:
+        write = csv.writer(csvnew)
+        for line in station_member:
+             write.writerow(line)
+    return station_member
 
 
-def random_node(node_member, len_nodes, width, height, station):
+def random_node(node_member, len_nodes, width, height, station_member):
     """random Node"""
     count = 0
     while len(node_member) != len_nodes:
         random_x, random_y = rd.randint(0, width), rd.randint(0, height)
         if [random_x, random_y] not in node_member and \
-                [random_x, random_y] not in station:
+                [random_x, random_y] not in station_member:
             node_member.append([random_x, random_y])
         count += 1
-    # split 2d list to 1d list
-    node_x, node_y = zip(*node_member)
-    return node_member, node_x, node_y
+    # append data to csv. file
+    with open('node_member.csv', 'w', newline='') as csvnew:
+        write = csv.writer(csvnew)
+        for line in node_member:
+            write.writerow(line)
+    return node_member
 
 
 def random_cluster(cluster_member, len_cluster, node_member):
     """random Cluster from amount Node"""
-    count = 0
     while len(cluster_member) != len_cluster:
         cluster = node_member[rd.randint(0, len(node_member) - 1)]
 
-        if count < 2 and cluster not in cluster_member:
+        if cluster not in cluster_member:
             cluster_member.append(cluster)
-            node_member.remove(cluster)
-        elif cluster not in cluster_member:
-            cluster_member.append(cluster)
-            node_member.remove(cluster)
-        count += 1
-    # split 2d list to 1d list
-    cluster_x, cluster_y = zip(*cluster_member)
-    return cluster_x, cluster_y, cluster_member
+    # append data to csv. file
+    with open('cluster_member.csv', 'w', newline='') as csvnew:
+        write = csv.writer(csvnew)
+        for line in cluster_member:
+            write.writerow(line)
+    return cluster_member
 
 
 def cal_shot_distance(node_member, cluster_member, shot_dis_data):
@@ -69,52 +72,100 @@ def cal_shot_distance(node_member, cluster_member, shot_dis_data):
                 shot_dis = cal_distance
                 what_cluster = cluster
         shot_dis_data.append([node, what_cluster, shot_dis])
+        # append data to csv. file
+        with open('shot_dis_data.csv', 'w', newline='') as csvnew:
+            write = csv.writer(csvnew)
+            for line in shot_dis_data:
+                write.writerow(line)
     return shot_dis_data
 
 
-def plot(shot_dis_data, node_member, cluster_member, clus_x, clus_y, base_x, base_y, node_x, node_y):
+def plot(shot_dis_data, node_member, cluster_member, station_member):
     """plot everything in graph"""
     # plot line between node and cluster
     for z in range(len(shot_dis_data)):
-        plt.plot([node_member[shot_dis_data[z][0]][0], cluster_member[shot_dis_data[z][1]][0]],
-                 [node_member[shot_dis_data[z][0]][1], cluster_member[shot_dis_data[z][1]][1]],
-                 color='k', linestyle='-', linewidth=0.1)  # Black Line
+        if shot_dis_data[z][2] != 0:
+            plt.plot([node_member[shot_dis_data[z][0]][0], cluster_member[shot_dis_data[z][1]][0]],
+                     [node_member[shot_dis_data[z][0]][1], cluster_member[shot_dis_data[z][1]][1]],
+                     color='k', linestyle='-', linewidth=0.1)  # Black Line
+    # split 2d list to 1d list
+    print(station_member)
+    print(cluster_member)
+    print(node_member)
+    base_x, base_y = zip(*station_member)
+    clus_x, clus_y = zip(*cluster_member)
+    node_x, node_y = zip(*node_member)
     # plot node, base, cluster
+    plt.xlabel('Width')
+    plt.ylabel('Height')
+    plt.title('Random Sensor')
+    plt.grid(True)
     plt.plot(base_x[0:], base_y[0:], 'ro')  # base station
     plt.plot(node_x[0:], node_y[0:], 'bo')  # nodes
     plt.plot(clus_x[0:], clus_y[0:], 'go')  # cluster head
     plt.show()
 
 
-def main(width, height, density, clus_density, num_base):
-    """insert area and population of node and point of base station"""
-
+def new_input(width, height, density, cluster_density, num_base):
+    """insert area and population of node and point of base station""" \
+ \
     # variable
-    node_member, cluster_member, station, shot_dis_data, len_nodes, len_cluster = variable(width, height, density,
-                                                                                           clus_density)
-
-    # set base_station
-    station, base_x, base_y = base_station(num_base, station)
+    node_member, cluster_member, station_member, shot_dis_data, len_nodes, len_cluster = variable(width, height,
+                                                                                                  density,
+                                                                                                  cluster_density)
 
     # random_node
-    node_member, node_x, node_y = random_node(node_member, len_nodes, width, height, station)
+    node_member = random_node(node_member, len_nodes, width, height, station_member)
 
     # random_cluster
-    cluster_x, cluster_y, cluster_member = random_cluster(cluster_member, len_cluster, node_member)
+    cluster_member = random_cluster(cluster_member, len_cluster, node_member)
+
+    # set base_station
+    station_member = base_station(num_base, station_member)
 
     # cal_shot_distance
     shot_dis_data = cal_shot_distance(node_member, cluster_member, shot_dis_data)
 
     # plot
-    plot(shot_dis_data, node_member, cluster_member, cluster_x, cluster_y, base_x, base_y, node_x, node_y)
+    plot(shot_dis_data, node_member, cluster_member, station_member)
 
 
-main(int(50),
-     int(50),
-     float(0.025),
-     float(0.079),
-     int(1))
+def only_random_cluster():
+    shot_dis_data, node_member, cluster_member, station_member = [], [], [], []
+    with open("shot_dis_data.csv", 'r') as csvnew:
+        read = csv.reader(csvnew)
+        for line in read:
+            shot_dis_data.append(line)
+    with open("node_member.csv", 'r') as csvnew:
+        read = csv.reader(csvnew)
+        for line in read:
+            node_member.append(line)
+    with open("cluster_member.csv", 'r') as csvnew:
+        read = csv.reader(csvnew)
+        for line in read:
+            cluster_member.append(line)
+    with open("station_member.csv", 'r') as csvnew:
+        read = csv.reader(csvnew)
+        for line in read:
+            station_member.append(line)
+    print(shot_dis_data)
+    print(cluster_member)
+    print(station_member)
+    print(shot_dis_data)
 
+
+def start():
+    """It's all begin here..."""
+    option = int(input("0,1,2 : "))
+    if option == 0:  # new input
+        new_input(int(50),
+                  int(50),
+                  float(0.025),
+                  float(0.079),
+                  int(1))
+    elif option == 1:  # random cluster:
+        only_random_cluster()
+start()
 # input("Width of this area (Meter) = ")
 # input("Height of this area (Meter) = ")
 # input("Node density (Node/Meter^2) = ")
