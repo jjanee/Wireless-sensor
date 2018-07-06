51e768fa97c1e58a4abe50c6392cfd2fc18712ff
max11357 <max7456@gmail.com>
2018-07-06 17:13:39 +0700

    make random complete
    - แรนด้อมโดยยังไม่เอาพลังงานมาเกี่ยวข้อง
    - เซฟภาพอัตโนมัติ ปิดให้ด้วย
    - เซฟทับ .csv กับ list data  เพราะยังไม่รู้จะเอาไปใช้ทำอะไร

--- max_cp_SetInput.py
+++ max_cp_SetInput.py
@@ -20,7 +20,7 @@ def base_station(num_base, station_member):
     with open('station_member.csv', 'w', newline='') as csvnew:
         write = csv.writer(csvnew)
         for line in station_member:
-             write.writerow(line)
+            write.writerow(line)
     return station_member
 
 
@@ -29,8 +29,7 @@ def random_node(node_member, len_nodes, width, height, station_member):
     count = 0
     while len(node_member) != len_nodes:
         random_x, random_y = rd.randint(0, width), rd.randint(0, height)
-        if [random_x, random_y] not in node_member and \
-                [random_x, random_y] not in station_member:
+        if [random_x, random_y] not in node_member and [random_x, random_y] not in station_member:
             node_member.append([random_x, random_y])
         count += 1
     # append data to csv. file
@@ -41,22 +40,41 @@ def random_node(node_member, len_nodes, width, height, station_member):
     return node_member
 
 
-def random_cluster(cluster_member, len_cluster, node_member):
+def random_cluster(cluster_member, len_cluster, node_member, option, shot_dis_data):
     """random Cluster from amount Node"""
-    while len(cluster_member) != len_cluster:
-        cluster = node_member[rd.randint(0, len(node_member) - 1)]
-        if cluster not in cluster_member:
-            cluster_member.append(cluster)
-    # append data to csv. file
-    with open('cluster_member.csv', 'w', newline='') as csvnew:
-        write = csv.writer(csvnew)
-        for line in cluster_member:
-            write.writerow(line)
+    if option == 0:
+        while len(cluster_member) != len_cluster:
+            cluster = node_member[rd.randint(0, len(node_member) - 1)]
+            if cluster not in cluster_member:
+                cluster_member.append(cluster)
+        # append data to csv. file
+        with open('cluster_member.csv', 'w', newline='') as csvnew:
+            write = csv.writer(csvnew)
+            for line in cluster_member:
+                write.writerow(line)
+    elif option == 2:
+        cluster_member = []
+        count = 0
+        while count != len_cluster:
+            cluster = node_member[rd.randint(0, len(node_member) - 1)]
+            for i in range(len(shot_dis_data)):
+                if int(shot_dis_data[i][1]) == count and float(
+                        shot_dis_data[i][2]) != 0.0 and cluster not in cluster_member:
+                    cluster_member.append(cluster)
+                    print(cluster)
+                    count += 1
+        # append data to csv. file
+        with open('cluster_member.csv', 'w', newline='') as csvnew:
+            write = csv.writer(csvnew)
+            for line in cluster_member:
+                write.writerow(line)
     return cluster_member
 
 
-def cal_shot_distance(node_member, cluster_member, shot_dis_data):
+def cal_shot_distance(node_member, cluster_member, shot_dis_data, option):
     """find distance between node and cluster"""
+    if option == 2:  # ***** important for loop
+        shot_dis_data = []
     for node in range(len(node_member)):
         shot_dis = None  # shortest distance
         what_cluster = None  # what cluster?
@@ -71,17 +89,18 @@ def cal_shot_distance(node_member, cluster_member, shot_dis_data):
                 shot_dis = cal_distance
                 what_cluster = cluster
         shot_dis_data.append([node, what_cluster, shot_dis])
-        # append data to csv. file
-        with open('shot_dis_data.csv', 'w', newline='') as csvnew:
-            write = csv.writer(csvnew)
-            for line in shot_dis_data:
-                write.writerow(line)
+
+    with open('shot_dis_data.csv', 'w', newline='') as csvnew:
+        write = csv.writer(csvnew)
+        for line in shot_dis_data:
+            write.writerow(line)
     return shot_dis_data
 
 
-def plot(shot_dis_data, node_member, cluster_member, station_member):
+def plot(shot_dis_data, node_member, cluster_member, station_member, count_lap, option):
     """plot everything in graph"""
     # plot line between node and cluster
+    plt.ion()  # make plt.close() can executed ***don't delete please
     for z in range(len(shot_dis_data)):
         if shot_dis_data[z][2] != 0:
             plt.plot([node_member[int(shot_dis_data[z][0])][0], cluster_member[int(shot_dis_data[z][1])][0]],
@@ -100,13 +119,18 @@ def plot(shot_dis_data, node_member, cluster_member, station_member):
     plt.plot(base_x[0:], base_y[0:], 'ro', markersize=3)  # base station
     plt.plot(node_x[0:], node_y[0:], 'bo', markersize=3)  # nodes
     plt.plot(clus_x[0:], clus_y[0:], 'go', markersize=3)  # cluster head
+    #  how to rename files
+    if option == 0 or option == 1:
+        plt.savefig("Figure_%s.png" % count_lap)
+    elif option == 2:
+        plt.savefig("Figure_%d.png" % count_lap)
     plt.show()
+    plt.close()
 
 
+def new_input(width, height, density, cluster_density, num_base, option):
+    """insert area and population of node and point of base station"""
 
-def new_input(width, height, density, cluster_density, num_base):
-    """insert area and population of node and point of base station""" 
-    
     # variable
     node_member, cluster_member, station_member, shot_dis_data, len_nodes, len_cluster = variable(width, height, density
                                                                                                   , cluster_density)
@@ -115,23 +139,23 @@ def new_input(width, height, density, cluster_density, num_base):
     node_member = random_node(node_member, len_nodes, width, height, station_member)
 
     # random_cluster
-    cluster_member = random_cluster(cluster_member, len_cluster, node_member)
+    cluster_member = random_cluster(cluster_member, len_cluster, node_member, option, shot_dis_data)
 
     # set base_station
     station_member = base_station(num_base, station_member)
 
     # cal_shot_distance
-    shot_dis_data = cal_shot_distance(node_member, cluster_member, shot_dis_data)
-
+    shot_dis_data = cal_shot_distance(node_member, cluster_member, shot_dis_data, option)
 
     # plot
-    plot(shot_dis_data, node_member, cluster_member, station_member)
+    count_lap = "NEW_INPUT"
+    plot(shot_dis_data, node_member, cluster_member, station_member, count_lap, option)
 
 
-def random_cluster_ingroup():
+def random_cluster_ingroup(option, lap):
     """only random new cluster from their own group"""
-    #gain data from .csv files
-    shot_dis_data, node_member, cluster_member, station_member = [], [], [], []
+    # gain data from .csv files
+    old_sdd, old_nm, old_cm, station_member = [], [], [], []
     with open("station_member.csv", 'r') as csvnew:
         read = csv.reader(csvnew)
         for line in read:
@@ -139,25 +163,37 @@ def random_cluster_ingroup():
     with open("node_member.csv", 'r') as csvnew:
         read = csv.reader(csvnew)
         for line in read:
-            node_member.append(list(map(int, line)))
+            old_nm.append(list(map(int, line)))
     with open("cluster_member.csv", 'r') as csvnew:
         read = csv.reader(csvnew)
         for line in read:
-            cluster_member.append(list(map(int, line)))
+            old_cm.append(list(map(int, line)))
     with open("shot_dis_data.csv", 'r') as csvnew:
         read = csv.reader(csvnew)
         for line in read:
-            shot_dis_data.append(line)
+            old_sdd.append(line)
+
+    # loop with lap input
+    len_cluster = len(old_cm)
+    for count_lap in range(lap):
+        # random_cluster
+        cluster_member = random_cluster(old_cm, len_cluster, old_nm, option, old_sdd)
+
+        # cal_shot_distance
+        shot_dis_data = cal_shot_distance(old_nm, cluster_member, old_sdd, option)
+
+        # plot
+        plot(shot_dis_data, old_nm, cluster_member, station_member, count_lap, option)
+
     # sort by group
     # shot_dis_data.sort(key=lambda x: int(x[1]))
     # for x in range(len(shot_dis_data)):
     #     print(shot_dis_data[x])
 
 
-
-def current_data():
+def current_data(option):
     """use current data not change anything"""
-    #gain data from .csv files
+    # gain data from .csv files
     shot_dis_data, node_member, cluster_member, station_member = [], [], [], []
     with open("station_member.csv", 'r') as csvnew:
         read = csv.reader(csvnew)
@@ -176,25 +212,26 @@ def current_data():
         for line in read:
             shot_dis_data.append(line)
     # plot
-    plot(shot_dis_data, node_member, cluster_member, station_member)
+    count_lap = "CURRENT_DATA"
+    plot(shot_dis_data, node_member, cluster_member, station_member, count_lap, option)
 
 
 def start():
     """Choose Functions"""
-    option = int(input("0,1,2 : "))
+    print("Choose 0 new input")
+    print("Choose 1 current data")
+    print("Choose 2 random only cluster")
+    option = int(input("----> "))
     if option == 0:  # new input
-        new_input(int(50),
-                  int(50),
-                  float(0.025),
-                  float(0.079),
-                  int(1))
+        new_input(int(50), int(50), float(0.025), float(0.079), int(1), option)
     elif option == 1:  # current data:
-        current_data()
+        current_data(option)
     elif option == 2:
-        random_cluster_ingroup()
+        lap = int(input("how many lap do you need? : "))
+        random_cluster_ingroup(option, lap)
+
 
 start()
-
 
 # input("Width of this area (Meter) = ")
 # input("Height of this area (Meter) = ")
